51d00b600bdea9ed6bdb7925e28fdba63b158c16
Chris-TopherW <christhewratt@gmail.com>
2016-09-20 16:12:02 +1200

    spinnin mac

---
+++ SwimSwimSwim/Assets/Scripts/SpinningObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningObject : MonoBehaviour {
+
+	public int speed = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (Vector3.down * Time.deltaTime * speed);
+	}
+}
