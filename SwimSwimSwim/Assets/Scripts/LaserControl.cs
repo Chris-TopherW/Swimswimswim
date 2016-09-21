5c7306630c0d7a6cc97f7523dd9c3ae31a30d786
Chris-TopherW <christhewratt@gmail.com>
2016-09-21 14:43:09 +1200

    Laser EYEEEES

---
+++ SwimSwimSwim/Assets/Scripts/LaserControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserControl : MonoBehaviour {
+
+	LineRenderer line;
+	private Vector3 mousePosition;
+
+	void Start () 
+	{
+		line = GetComponent<LineRenderer> ();
+		line.enabled = false;
+		mousePosition = new Vector3(Input.mousePosition.x, 0f, Input.mousePosition.y);
+
+	}
+
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1"))
+		{
+			Debug.Log ("Fire!");
+			StopCoroutine ("FireLaser");
+			StartCoroutine ("FireLaser");
+		}
+	}
+
+	IEnumerator FireLaser()
+	{
+		line.enabled = true;
+		while (Input.GetButton ("Fire1")) 
+		{
+			mousePosition.x = Input.mousePosition.x - 100;
+			mousePosition.y = Input.mousePosition.y - 100;
+			Ray ray = new Ray (transform.position, mousePosition);
+			RaycastHit hit;
+
+			line.SetPosition (0, ray.origin);
+			if (Physics.Raycast (ray, out hit, 100)) 
+			{
+				line.SetPosition (1, hit.point);
+				Destroy (hit.transform.gameObject);
+			}
+			else
+				line.SetPosition (1, ray.GetPoint (100));
+
+			yield return null;
+		}
+		line.enabled = false;
+	}
+}
