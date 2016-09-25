5cc26b7db7308943bdcd2c5104186127ad07da26
kellymoen <kel.moen@gmail.com>
2016-09-26 00:55:25 +1300

    Procedural Mesh generation along splinez

---
+++ SwimSwimSwim/Assets/Scripts/ProceduralMesh.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralMesh : MonoBehaviour {
+
+    // Use this for initialization
+    void OnDrawGizmos() {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter.sharedMesh == null)
+            meshFilter.sharedMesh = new Mesh();
+        Mesh mesh = meshFilter.sharedMesh;
+
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(1,0,1),
+            new Vector3(0,0,1),
+            new Vector3(0,0,0),
+            new Vector3(1,0,0)
+        };
+        Vector3[] normals = new Vector3[]
+        {
+            Vector3.up,
+            Vector3.up,
+            Vector3.up,
+            Vector3.up
+        };
+        Vector2[] uvs = new Vector2[]
+        {
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+            new Vector2(1, 0)
+        };
+        int[] triangles = new int[]
+        {
+            2,1,0,
+            0,3,2
+        };
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.normals = normals;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+            
+            
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
