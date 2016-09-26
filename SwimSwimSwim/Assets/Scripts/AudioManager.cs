8107e0102d68ff04e2aef9487d173ab203cca4e2
Chris-TopherW <christhewratt@gmail.com>
2016-09-27 12:23:33 +1300

    Full audio control woooooop

--- SwimSwimSwim/Assets/Scripts/AudioManager.cs
+++ SwimSwimSwim/Assets/Scripts/AudioManager.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GAudio;
 
-public class AudioManager : MonoBehaviour {
+public class AudioManager : MonoBehaviour, IGATPulseClient 
+{
+	public PulseModule pulse;
+	public GameObject patterModuleObjectLeft;
+	public GameObject patterModuleObjectRight;
 
-	// Use this for initialization
-	void Start () {
-	
+	private PulsedPatternModule pulsedPatternLeft;
+	private PulsedPatternModule pulsedPatternRight;
+	private int loopNumber = 0;
+
+	public void OnEnable()
+	{
+		pulse.SubscribeToPulse (this);
+		pulsedPatternLeft = patterModuleObjectLeft.GetComponent<PulsedPatternModule> ();
+		pulsedPatternRight = patterModuleObjectRight.GetComponent<PulsedPatternModule> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void OnDisable()
+	{
+		pulse.UnsubscribeToPulse (this);
 	}
+
+	public void OnPulse(IGATPulseInfo pulseInfo)
+	{
+		print (pulseInfo.StepIndex + "Pulse!");
+
+		if (loopNumber < 10) {
+			if (pulseInfo.StepIndex == 0) {
+
+				switch (loopNumber) {
+				case 1:
+					pulsedPatternLeft.AddSample ("AMelodyLoopVapor_0");
+					pulsedPatternRight.AddSample ("AMelodyLoopVapor_1");
+					pulsedPatternLeft.RemoveSampleAt (0);
+					pulsedPatternRight.RemoveSampleAt (0);
+					break;
+				case 2:
+					pulsedPatternLeft.AddSample ("ALoopVapor_0");
+					pulsedPatternRight.AddSample ("ALoopVapor_1");
+					pulsedPatternLeft.RemoveSampleAt (0);
+					pulsedPatternRight.RemoveSampleAt (0);
+					break;
+				case 3:
+					pulsedPatternLeft.AddSample ("BreakdownVapor_0");
+					pulsedPatternRight.AddSample ("BreakdownVapor_1");
+					pulsedPatternLeft.RemoveSampleAt (0);
+					pulsedPatternRight.RemoveSampleAt (0);
+					break;
+				case 4:
+					pulsedPatternLeft.AddSample ("BMelodyVapor_0");
+					pulsedPatternRight.AddSample ("BMelodyVapor_1");
+					pulsedPatternLeft.RemoveSampleAt (0);
+					pulsedPatternRight.RemoveSampleAt (0);
+					break;
+				}
+			}
+		}
+		if (pulseInfo.StepIndex == 0)
+			loopNumber++;
+
+	}
+
+	public void PulseStepsDidChange(bool[] newSteps)
+	{
+
+	}
+
 }
