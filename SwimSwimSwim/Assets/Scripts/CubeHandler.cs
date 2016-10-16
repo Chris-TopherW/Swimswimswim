2b74c527b650868f746ef4e2aa0dac5108acd5dd
kellymoen <kel.moen@gmail.com>
2016-10-16 22:40:00 +1300

    seperate script to control fire mechanic, spherecast for lack of touch precision

--- SwimSwimSwim/Assets/Scripts/CubeHandler.cs
+++ SwimSwimSwim/Assets/Scripts/CubeHandler.cs
@@ -7,18 +7,19 @@ using System.Linq;
 
 public class CubeHandler : MonoBehaviour, IGATPulseController
 {
-    public PulseModule                           pulse;
-    public PulsedPatternModule                   pattern;
-    public PulsedPatternModule                   firePattern;
-    public static List<CubeThumper>              hitCubes;
-    public static List<CubeThumper>              cubesToDestroy;
+    public static CubeHandler handler;
+    public PulseModule pulse;
+    public PulsedPatternModule pattern;
+    public PulsedPatternModule firePattern;
+    public static List<CubeThumper> hitCubes;
+    public static List<CubeThumper> cubesToDestroy;
     private int numCubesHit = 0;
     private int subPulseStepIndex = 0;
     private int masterStepIndex = 0;
     private int masterTotalSteps = 0;
     private int subPulseTotalSteps = 8;
 
-    private bool firing = false;
+    public static bool firing = false;
     private bool fireInit = false;
 
 
@@ -26,6 +27,7 @@ public class CubeHandler : MonoBehaviour, IGATPulseController
 
     void OnEnable()
     {
+        handler = this;
         //Subscribe and register, we need the info! Always in OnEnable
         pulse.RegisterPulseController(this);
     }
@@ -35,7 +37,7 @@ public class CubeHandler : MonoBehaviour, IGATPulseController
         pulse.UnregisterPulseController(this);
     }
 
-    public void OnPulseControl( IGATPulseInfo prevPulseInfo )
+    public void OnPulseControl(IGATPulseInfo prevPulseInfo)
     {
 
         //This info is needed to enable the next pulse -- even if the previous pulse was the last
@@ -46,7 +48,8 @@ public class CubeHandler : MonoBehaviour, IGATPulseController
 
         //This block disables the previous pulse step, 
         //then sets the number of samples to play in the upcoming pulse to the number of cubes locked on since the last pulse
-        if (true) {
+        if (true)
+        {
             pattern.SubscribedSteps[subPulseStepIndex] = false;
             if (numCubesHit > 0)
             {
@@ -54,58 +57,34 @@ public class CubeHandler : MonoBehaviour, IGATPulseController
                 pattern.SubscribedSteps[(subPulseStepIndex + 1) % subPulseTotalSteps] = true;
             }
             numCubesHit = 0;
-
-
-            if (subPulseStepIndex == 0)
-            {
-                firePattern.SubscribedSteps[(masterStepIndex)] = false;
-            }
         }
-        /*
-         * We're firing here -- we're using the master pulse currently, so we only want this code to run on the first sub pulse
-         * Again, the previous pulse is disabled for the fire pattern.
-         * The first cube in the list gets passed the pulseIndex that the destroy animation should trigger on, and is then removed.
-         * This ensures that the cubes are destroyed in the order that they were originally selected.
-         * 
-         * TODO: A more complicated destroy pattern. Probably move this to a second script with another master pulse at 4X BPM to manually
-         *       craft a destroy rhythm that isn't just on every beat and allows for more variety than just another subPulse.
-         */
-        if (firing && cubesToDestroy.Count != 0 && subPulseStepIndex == 0)
-        {
-            firePattern.SubscribedSteps[masterStepIndex] = false;
-            firePattern.SubscribedSteps[(masterStepIndex + 1) % masterTotalSteps] = true;
-            cubesToDestroy[0].hitIndex = (masterStepIndex + 1) % masterTotalSteps;
-            cubesToDestroy[0].isFiring = true;
-            cubesToDestroy.Remove(cubesToDestroy[0]);
-        } else
-        {
-            if (cubesToDestroy.Count == 0)
-            {
-                firing = false;
-            }
-        }
+
 
 
     }
 
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         hitCubes = new List<CubeThumper>();
 
         cubesToDestroy = new List<CubeThumper>();
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
-        RaycastHit hit;
-        if ( Physics.Raycast( ray, out hit, 1000.0f ) ) {
-            GameObject obj = hit.transform.gameObject;
-            if ( obj.CompareTag ("Thumper") ) {
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.SphereCastAll(ray.origin, 1, ray.direction, 1000.0f);
+        for (int i = 0; i < hits.Length && i < 1; i++)
+        {
+            GameObject obj = hits[i].transform.gameObject;
+            if (obj.CompareTag("Thumper"))
+            {
 
                 CubeThumper thump = obj.GetComponent<CubeThumper>();
-                if ( !thump.IsTriggered() && hitCubes.Count < 8 && !cubesToDestroy.Contains(thump)){
+                if (!thump.IsTriggered() && hitCubes.Count < 8 && !cubesToDestroy.Contains(thump))
+                {
                     thump.isTriggered = true;
                     hitCubes.Add(thump);
                     numCubesHit++;
@@ -113,7 +92,8 @@ public class CubeHandler : MonoBehaviour, IGATPulseController
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && !firing) {
+        if ((Input.touchCount > 1 || Input.GetKeyDown(KeyCode.Space)) && !firing)
+        {
             firing = true;
             for (int i = 0; i < hitCubes.Count; i++)
             {
@@ -122,5 +102,5 @@ public class CubeHandler : MonoBehaviour, IGATPulseController
             hitCubes.Clear();
 
         }
-	}
+    }
 }
--- SwimSwimSwim/Assets/Scripts/CubeThumper.cs
+++ SwimSwimSwim/Assets/Scripts/CubeThumper.cs
@@ -6,19 +6,23 @@ using GAudio;
     public class CubeThumper : MonoBehaviour
     {
     public PulseModule pulse;
+    public PulseModule firePulse;
     public int hitIndex;
     public bool isTriggered, isFiring, hasFired;
 
         void OnEnable()
         {
         pulse = GameObject.Find("Sub Pulse").GetComponent<SubPulseModule>();
-        pulse.onWillPulse += OnPulse;
+        firePulse = GameObject.Find("FirePulse").GetComponent<SubPulseModule>();
+        pulse.onWillPulse += OnTriggerPulse;
+        firePulse.onWillPulse += OnFirePulse;
         }
    
     void OnDisable()
         {
-        pulse.onWillPulse -= OnPulse;
-        }
+        pulse.onWillPulse -= OnTriggerPulse;
+        firePulse.onWillPulse -= OnFirePulse;
+    }
 
     public bool IsTriggered()
     {
@@ -26,14 +30,14 @@ using GAudio;
     }
 
 
-   private void OnPulse(IGATPulseInfo pulseInfo)
+   private void OnTriggerPulse(IGATPulseInfo pulseInfo)
     {
-        if (hasFired && pulseInfo.StepIndex == pulseInfo.NbOfSteps-1)
-        {
-            Destroy(gameObject);
-        }
         if (isTriggered && !hasFired) {
             GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        }
+        if (hasFired && pulseInfo.StepIndex == pulseInfo.NbOfSteps - 1)
+        {
+            Destroy(gameObject);
         }
         if (isFiring && pulseInfo.PulseSender.MasterPulseInfo.StepIndex == hitIndex)
         {
@@ -42,6 +46,21 @@ using GAudio;
             hasFired = true; //TODO: This is gross -- change to enums.
         }
     }
+
+    private void OnFirePulse(IGATPulseInfo pulseInfo)
+    {
+        if (hasFired && pulseInfo.StepIndex == (hitIndex + 1) % pulseInfo.NbOfSteps)
+        {
+            Destroy(gameObject);
+        }
+        if (isFiring && pulseInfo.StepIndex == hitIndex)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+            isFiring = false;
+            hasFired = true; //TODO: This is gross -- change to enums.
+        }
+    }
+
 
 
     // Use this for initialization
