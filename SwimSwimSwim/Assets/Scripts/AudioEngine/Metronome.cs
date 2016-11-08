a8291b19bde674a7b18e587a2bc1cf5c5fd95a8c
Chris-TopherW <wratt_c@actrix.co.nz>
2016-11-08 16:17:57 +1300

    custom audio engine code added

---
+++ SwimSwimSwim/Assets/Scripts/AudioEngine/Metronome.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Metronome class as sample accurate clock to run instruments.
+/// </summary>
+
+public class Metronome : MonoBehaviour
+{
+	//instantiate static object to access from other scripts
+	public static Metronome metro;
+
+	public float BPM = 120.0f;
+	[HideInInspector]
+	public int currentTick, currentBar, samplesPerTick, ticksPerBar;
+	[HideInInspector]
+	public bool ready;
+
+	private int samplesPerQuarter, phasor, numberOfHits,
+	DSPBufferSize, DSPNumBuffers, sampleRate, ticksPerQuarter, quartersPerBar;
+
+	void Awake () 
+	{
+		//wait for instrument set up
+		ready = false;
+			metro = this;
+
+		//set up audio DSP buffer size and sample rate for whole program
+		AudioSettings.GetDSPBufferSize (out DSPBufferSize, out DSPNumBuffers);
+		sampleRate = AudioSettings.outputSampleRate;
+
+		ticksPerQuarter = 8;
+		quartersPerBar = 4;
+		ticksPerBar = ticksPerQuarter * quartersPerBar;
+		//set up basic subdivision
+		samplesPerQuarter = (int)(sampleRate / (BPM / 60.0f));
+		samplesPerTick = (int)(samplesPerQuarter / ticksPerQuarter);
+		currentTick = 0;
+		currentBar = 1;
+	}
+
+	void OnAudioFilterRead(float[] samples, int channels)
+	{
+		for (int i = 0; i < DSPBufferSize; i++) 
+		{
+			phasor++;
+			if (phasor == samplesPerTick) {
+				phasor = 0;
+				if (currentTick == quartersPerBar * ticksPerQuarter - 1) {
+					currentTick = 0;
+					currentBar++;
+				}
+				else
+					currentTick++;
+			}
+		}
+	}
+
+	public int getDSPBufferSize()
+	{
+		return DSPBufferSize;
+	}
+
+	public int getSampleRate()
+	{
+		return sampleRate;
+	}
+}
---
+++ SwimSwimSwim/Assets/Scripts/AudioEngine/SequencerInstrument.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+/// <summary>
+/// Instrument voice class for audio sequencer.
+/// </summary>
+
+public class SequencerInstrument : MonoBehaviour
+{	
+	public AudioClip audioClip;
+	public AudioSource audioSource;
+	public AudioMixer mixer;
+
+	[HideInInspector]
+	public bool[] score;
+
+	private int numSamples, DSPBufferingSize, playhead, phasor;
+	private float gain = 1.0f;
+	private float[] sampleBuffer;
+	private bool processAudio;
+
+	void Awake()
+	{
+		audioSource = GetComponent<AudioSource> ();
+		score = new bool[Metronome.metro.ticksPerBar];
+	}
+
+	void Start ()
+	{
+		numSamples = audioClip.samples;
+		DSPBufferingSize = Metronome.metro.getDSPBufferSize ();
+
+		//setup mixer output
+		string _OutputMixer = ("Channel" + this.name);       
+		audioSource.outputAudioMixerGroup = mixer.FindMatchingGroups(_OutputMixer)[0];
+
+		//setup input buffer to store clip of floats and set to end of buffer
+		sampleBuffer = new float[numSamples];
+		audioClip.GetData (sampleBuffer, 0);
+		playhead = numSamples - 1;
+		//new GameObject
+
+		//wait for everything to be set up then start metro
+		Metronome.metro.ready = true;
+	}
+
+	//to place samples in specific channels
+	// i % channels == x, where x is channel from 0 to channels -1,
+	// For example if audio is stereo, then channel 0 will be left, channel 1 will be right so
+	// i % channels == 0 will hold true on left channel.
+	void OnAudioFilterRead(float[] samples, int channels)
+	{
+		for (int i = 0; i < DSPBufferingSize; i++) 
+		{
+			phasor++;
+			if (playhead < numSamples - 1)
+				playhead++;
+
+			if (phasor == Metronome.metro.samplesPerTick) {
+				phasor = 0;
+
+				if(score[Metronome.metro.currentTick])
+					playhead = 0;
+				}
+			if (playhead < numSamples -1){
+				samples [i] = sampleBuffer[playhead] * gain;
+			} else {
+				samples[i] = 0;
+			}
+		}
+	}
+}
