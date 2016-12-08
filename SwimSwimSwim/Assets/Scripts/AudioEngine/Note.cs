d0487ba90867a21703f3e428f1d2e4162505f5e3
kellymoen <kel.moen@gmail.com>
2016-12-08 15:24:14 +1300

    Looping notes in perf time chyea chyea

--- SwimSwimSwim/Assets/Scripts/AudioEngine/Note.cs
+++ SwimSwimSwim/Assets/Scripts/AudioEngine/Note.cs
@@ -7,22 +7,49 @@ public class Note
     public int quarter;
     public int tick;
     public AudioClip clip;
-    private AudioSource source;
+    private AudioSource[] sources;
     private Metronome metro;
-    
+    private NotationTime loop;
+    private NotationTime timeToPlay;
+    private double nextPlay;
+    public GameObject sequencer;
+    public int nextSource = 0;
+    public int lastSource = 0;
 
-    public Note(Metronome metro, NotationTime time, AudioClip clip, AudioSource source)
+    public Note(Metronome metro, NotationTime time, NotationTime loop, AudioClip clip, GameObject sequencer)
     {
+        sources = new AudioSource[2];
+        for (int i =0; i < sources.Length; i++)
+        {
+            sources[i] = sequencer.AddComponent < AudioSource >() as AudioSource;
+        }
         this.metro = metro;
         this.bar = time.bar;
         this.quarter = time.quarter;
         this.tick = time.tick;
         this.clip = clip;
-        this.source = source;
-        source.clip = clip;
-        source.volume = 1;
-        double nextPlay = metro.GetFutureTime(bar, quarter, tick);
-        source.PlayScheduled(nextPlay);
+        this.timeToPlay = time;
+        this.loop = loop;
+        this.sequencer = sequencer;
+        sources[nextSource].clip = clip;
+        sources[nextSource].volume = 1;
+        nextPlay = metro.GetFutureTime(bar, quarter, tick);
+        sources[nextSource].PlayScheduled(nextPlay);
+        nextSource = (lastSource + 1) % sources.Length;
+    }
+
+    public void Update()
+    {
+        if (loop.loops() && AudioSettings.dspTime > nextPlay)
+        {
+            timeToPlay.Add(loop);
+            sources[nextSource].clip = clip;
+            sources[nextSource].volume = 1;
+            nextPlay = metro.GetFutureTime(timeToPlay.bar, timeToPlay.quarter, timeToPlay.tick);
+            sources[nextSource].PlayScheduled(nextPlay);
+            lastSource = nextSource;
+            nextSource = (lastSource + 1) % sources.Length;
+        }
     }
 
 }
--- SwimSwimSwim/Assets/Scripts/AudioEngine/Sequence.cs
+++ SwimSwimSwim/Assets/Scripts/AudioEngine/Sequence.cs
@@ -17,37 +17,30 @@ public class Sequence : MonoBehaviour
     {
         metro = GameObject.FindGameObjectWithTag("Metronome").GetComponent<Metronome>();
         sources = new AudioSource[16];
-        for (int i = 0; i < sources.Length; i++)
-        {
-            sources[i] = gameObject.AddComponent<AudioSource>() as AudioSource;
-        }
-        notes = new Note[4];
-        NotationTime n1, n2, n3, n4;
-        n1 = new NotationTime();
-        n1.bar = 0;
-        n1.quarter = 0;
-        n1.tick = 6;
-        n2 = new NotationTime();
-        n2.bar = 0;
-        n2.quarter = 1;
-        n2.tick = 0;
-        n3 = new NotationTime();
-        n3.bar = 0;
-        n3.quarter = 1;
-        n3.tick = 2;
-        n4 = new NotationTime();
-        n4.bar = 0;
-        n4.quarter = 1;
-        n4.tick = 4;
-        notes[0] = new Note(metro, n1, clipsToPlay[0], sources[0]);
-        notes[1] = new Note(metro, n2, clipsToPlay[1], sources[1]);
-        notes[2] = new Note(metro, n3, clipsToPlay[2], sources[2]);
-        notes[3] = new Note(metro, n4, clipsToPlay[3], sources[3]);
-    }
+        notes = new Note[5];
+        NotationTime n1, n2, n3, n4, n5, barLoop, noLoop;
+        n1 = new NotationTime(0,0,7);
+        n2 = new NotationTime(0, 1, 1);
+        n3 = new NotationTime(0, 1, 3);
+        n4 = new NotationTime(0, 1, 5);
+        n5 = new NotationTime(0, 0, 1);
+        noLoop = new NotationTime(0, 0, 0);
+        barLoop = new NotationTime(1, 0, 0);
+       
+        notes[0] = new Note(metro, n1, barLoop, clipsToPlay[0], gameObject);
+        notes[1] = new Note(metro, n2, barLoop, clipsToPlay[1], gameObject);
+        notes[2] = new Note(metro, n3, barLoop, clipsToPlay[2], gameObject);
+        notes[3] = new Note(metro, n4, barLoop, clipsToPlay[3], gameObject);
+        notes[4] = new Note(metro, n5, barLoop, clipsToPlay[4], gameObject);
+ }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < notes.Length; i++)
+        {
+            notes[i].Update();
+        }
     }
 }
 
@@ -57,8 +50,33 @@ public struct NotationTime
     public int quarter;
     public int tick;
 
+    public NotationTime(int bar, int quarter, int tick)
+    {
+        this.bar = bar;
+        this.quarter = quarter;
+        this.tick = tick;
+    }
+
+    public bool loops()
+    {
+        return (bar != 0 || quarter != 0 || tick != 0);
+    }
+
     public void Add(NotationTime other)
     {
+        tick += other.tick;
+        if (tick >=8 )
+        {
+            tick -= 8;
+            quarter++;
+        }
+        quarter += other.quarter;
+        if (quarter >= 4)
+        {
+            quarter -= 4;
+            bar++;
+        }
+        bar += other.bar;
 
     }
 }
