2c0ea157cb76d5f8ebe9449a631215b30c927932
Chris-TopherW <wratt_c@actrix.co.nz>
2017-02-09 23:15:31 +1300

    prefabed up and integrated all new models

---
+++ SwimSwimSwim/Assets/Scripts/SpiningObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiningObject : MonoBehaviour {
+
+	public int speed = 50;
+	public bool vertical = false;
+	public bool horizontal = true;
+
+	void Update () {
+		if(horizontal) {
+		transform.Rotate (Vector3.down * Time.deltaTime * speed);
+		}
+		if(vertical) {
+			transform.Rotate (Vector3.left * Time.deltaTime * speed);
+		}
+	}
+}
