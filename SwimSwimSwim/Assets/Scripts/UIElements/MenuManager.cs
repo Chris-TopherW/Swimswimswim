a20e96d6da2751a7d28dab540974b3c70957f4f4
Chris-TopherW <wrattchri1@AD.ECS.VUW.AC.NZ>
2017-04-27 15:25:44 +1200

    Basic main menu
    Only play and Exit work rn

---
+++ SwimSwimSwim/Assets/Scripts/UIElements/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void loadGame() {
+        SceneManager.LoadScene(1);
+    }
+    public void loadOptions() {
+
+    }
+    public void loadHighScore() {
+
+    }
+    public void loadCredits() {
+
+    }
+    public void loadExit() {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+}
