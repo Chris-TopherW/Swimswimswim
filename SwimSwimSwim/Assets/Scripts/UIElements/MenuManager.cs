820b3881ac73d43daa636808c24260423935e737
Chris-TopherW <wrattchri1@AD.ECS.VUW.AC.NZ>
2017-04-27 15:55:26 +1200

    Basic main menu complete

--- SwimSwimSwim/Assets/Scripts/UIElements/MenuManager.cs
+++ SwimSwimSwim/Assets/Scripts/UIElements/MenuManager.cs
@@ -6,28 +6,43 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
+    public GameObject mainMenuButtons;
+    public GameObject optionsButtons;
+    public GameObject creditsView;
+    public GameObject highScoreView;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
     public void loadGame() {
         SceneManager.LoadScene(1);
     }
     public void loadOptions() {
-
+        mainMenuButtons.SetActive(false);
+        optionsButtons.SetActive(true);
     }
     public void loadHighScore() {
-
+        mainMenuButtons.SetActive(false);
+        highScoreView.SetActive(true);
     }
     public void loadCredits() {
-
+        mainMenuButtons.SetActive(false);
+        creditsView.SetActive(true);
     }
     public void loadExit() {
         Application.Quit();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
+    public void loadMainMenu()
+    {
+        mainMenuButtons.SetActive(true);
+        optionsButtons.SetActive(false);
+        creditsView.SetActive(false);
+        highScoreView.SetActive(false);
     }
 
 }
